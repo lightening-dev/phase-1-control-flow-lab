0f6eef974dd98d24d2cdb6a39998a84277da60cb
Lightening Gibson <lighteningibson@gmail.com>
2021-11-04 11:02:34 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(ride){
+  
+
+    if (ride <= 400) {
+      return 'This one is on me!'
+    }
+    else if  (ride > 2000 && ride <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+    }
+    else (ride > 2500) 
+
+      return 'No can do.'
+    
+  
+  }
+    
+
+function ternaryCheckCity(city){
+  // console.log(city)
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  
+  switch(tip) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous': 
+      return 'Thank you.'
+    default: 
+        return 'Bye.'
+  
+    }
+
+   
 }
